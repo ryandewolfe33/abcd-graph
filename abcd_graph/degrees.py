88e076ebf26badfcb3d80c8aed6ed001a214cf5d
Ryan DeWolfe <ryandewolfe33@gmail.com>
2026-08-16 15:25:55 -0400

    Add degree assignment and splitting

--- abcd_graph/degrees.py
+++ abcd_graph/degrees.py
@@ -5,7 +5,7 @@ from numpy.random import Generator
 from numpy.typing import NDArray
 
 
-@njit
+@njit(cache=True)
 def _split_community_degree(
     community_degrees: NDArray[np.uint32],
     background_degrees: NDArray[np.uint32],
@@ -79,3 +79,240 @@ def split_degrees(
     )
 
     return community_degrees.tocsr(), background_degrees
+
+
+def _assign_outlier_degrees(
+    degrees: NDArray[np.uint32],
+    outlier_threshold: float,
+    n_outliers: int,
+    rng: Generator,
+) -> (NDArray[np.uint32], NDArray[np.uint32]):
+    available_indices = np.where(degrees < outlier_threshold)[0]
+    if len(available_indices) > n_outliers:
+        chosen_indices = rng.choice(available_indices, size=n_outliers, replace=False)
+    else:
+        chosen_indices = np.argsort(degrees)[:n_outliers]
+    outlier_degrees = degrees[chosen_indices]
+
+    remaining_mask = np.ones_like(degrees, dtype=np.bool)
+    remaining_mask[chosen_indices] = False
+    remaining_degrees = degrees[remaining_mask]
+
+    return outlier_degrees, remaining_degrees
+
+
+@njit(cache=True)
+def _assign_degrees(
+    degrees: NDArray[np.uint32],
+    n_coms: NDArray,
+    thresholds: NDArray[np.floating],
+    rng: Generator,
+    alpha: float = 0.0,
+) -> NDArray[np.uint32]:
+    assigned_degrees = np.empty_like(degrees)
+    open_nodes = np.arange(len(n_coms), dtype=np.uint32)[n_coms > 0]
+    n_coms_exp_alpha = n_coms.astype(np.float64) ** alpha
+    for d in degrees:
+        allowed_indices = np.where(d <= thresholds[open_nodes])[0]
+        if len(allowed_indices) == 0:
+            allowed_indices = np.where(
+                thresholds[open_nodes] == np.min(thresholds[open_nodes])
+            )[0]
+        allowed_nodes = open_nodes[allowed_indices]
+
+        # Choose an available node proportional to n_coms ** alpha
+        if len(allowed_nodes) > 1:
+            probs = n_coms_exp_alpha[allowed_nodes]
+            probs /= np.sum(probs)
+            cum_prob = np.cumsum(probs)
+            random_value = rng.uniform()
+            chosen_index = np.searchsorted(cum_prob, random_value)
+        else:
+            chosen_index = 0
+        assigned_degrees[allowed_nodes[chosen_index]] = d
+
+        # Remove assigned node and shorten list
+        open_nodes[allowed_indices[chosen_index]] = open_nodes[-1]
+        open_nodes = open_nodes[:-1]
+
+    return assigned_degrees
+
+
+@njit(cache=True)
+def _assign_degrees_with_alpha_search(
+    degrees: NDArray[np.uint32],
+    n_coms: NDArray[np.uint32],
+    thresholds: NDArray[np.floating],
+    rng: Generator,
+    rho: float,
+    rho_tol: float,
+    alpha_min: float,
+    alpha_max: float,
+    alpha_iters: int,
+):
+    # We known the sign of alpha will match the sign of rho
+    if rho > 0:
+        max_rho_degrees = _assign_degrees(
+            degrees,
+            n_coms,
+            thresholds,
+            rng,
+            alpha_max,
+        )
+        empirical_rho = np.corrcoef(max_rho_degrees, n_coms)[0, 1]
+        if empirical_rho < rho:  # Return best try
+            return max_rho_degrees
+        else:  # Prep search for positive alpha
+            alpha_min = 0
+
+    elif rho < 0:
+        min_rho_degrees = _assign_degrees(
+            degrees,
+            n_coms,
+            thresholds,
+            rng,
+            alpha_min,
+        )
+        empirical_rho = np.corrcoef(min_rho_degrees, n_coms)[0, 1]
+        if empirical_rho > rho:  # Return best try
+            return min_rho_degrees
+        else:  # Prep search for negative alpha
+            alpha_max = 0
+
+    # Default to alpha=0
+    assigned_degrees = _assign_degrees(
+        degrees,
+        n_coms,
+        thresholds,
+        rng,
+    )
+
+    # Binary search for best alpha
+    for _ in range(alpha_iters):
+        alpha = (alpha_max + alpha_min) / 2
+        assigned_degrees = _assign_degrees(
+            degrees,
+            n_coms,
+            thresholds,
+            rng,
+            alpha,
+        )
+        empirical_rho = np.corrcoef(assigned_degrees, n_coms)[0, 1]
+        if np.abs(empirical_rho - rho) < rho_tol:
+            return assigned_degrees
+        elif empirical_rho > rho:
+            alpha_max = alpha
+        else:
+            alpha_min = alpha
+
+    return assigned_degrees
+
+
+def assign_degrees(
+    degrees: NDArray[np.uint32],
+    membership_matrix: sp.csr_array,
+    xi: float,
+    rng: Generator,
+    rho: float = 0.0,
+    rho_tol: float = 0.01,
+    alpha_min: float = -60,
+    alpha_max: float = 60,
+    alpha_iters: int = 10,
+) -> NDArray[np.uint32]:
+    """Assign degrees to nodes.
+
+    Parameters
+    ----------
+    degrees: NDArray[np.uint32]
+        Array of available degrees in any ordering.
+
+    membership_matrix: sp.csr_array
+        n_communities x n_nodes sparse matrix for community membership.
+
+    xi: float
+        Proportion of each degree that will be assigned to the global
+        background graph.
+
+    rho: float
+        Target pearson correlation between node degree and number of
+        communities the node is in. Only used if there is overlap.
+
+    rho_tol: float
+        Tolerance on empirical rho to stop alpha search early.
+
+    alpha_min: float
+        Internal parameter used for correlation between degree and number of
+        communities.
+
+    alpha_max: float
+        Internal parameter used for correlation between degree and number of
+        communities.
+
+    alpha_iters: int
+        Number of times to try alphas in a binary search.
+
+    rng: Generator
+        Source of randomness.
+
+    Returns
+    -------
+    assigned_degrees: NDArray[np.uint32]
+        Array of degrees that is aligned with the membership matrix.
+
+    """
+    degrees = np.sort(degrees)[::-1]  # sort degrees descending
+    community_sizes = membership_matrix.sum(axis=1).astype(
+        np.uint32
+    )  # size of each community
+    membership_matrix = membership_matrix.tocsc()
+    n_coms = membership_matrix.sum(axis=0)  # number of communities per node
+    community_size_matrix = (
+        sp.diags_array(community_sizes, dtype=np.uint32) @ membership_matrix
+    )
+    min_com_sizes = community_size_matrix.min(axis=0, explicit=True).todense()
+
+    n = membership_matrix.shape[1]
+    n_outliers = np.sum(n_coms == 0)
+    n_inliers = n - n_outliers
+    outlier_threshold = (
+        (n_inliers / n) * np.sum(np.minimum(xi * degrees, 1)) + n_outliers - 1
+    )
+
+    assigned_degrees = np.empty_like(degrees)
+    outlier_mask = n_coms == 0
+    outlier_degrees, remaining_degrees = _assign_outlier_degrees(
+        degrees,
+        outlier_threshold,
+        n_outliers,
+        rng,
+    )
+    assigned_degrees[outlier_mask] = outlier_degrees
+
+    eta = np.sum(community_sizes) / n_inliers
+    expected_primary_community_sizes = community_sizes / eta
+    phi = 1 - n_inliers * xi / (n_inliers * xi + n_outliers) * np.sum(
+        np.power(expected_primary_community_sizes / n_inliers, 2)
+    )
+    thresholds = min_com_sizes[~outlier_mask] * n_coms[~outlier_mask] / (1 - xi * phi)
+
+    if rho == 0:
+        inlier_degrees = _assign_degrees(
+            remaining_degrees,
+            n_coms[~outlier_mask],
+            thresholds,
+            rng,
+        )
+    else:
+        inlier_degrees = _assign_degrees_with_alpha_search(
+            remaining_degrees,
+            n_coms[~outlier_mask],
+            thresholds,
+            rng,
+            rho,
+            rho_tol,
+            alpha_min,
+            alpha_max,
+            alpha_iters,
+        )
+    assigned_degrees[~outlier_mask] = inlier_degrees
+    return assigned_degrees
--- tests/test_degrees.py
+++ tests/test_degrees.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 import scipy.sparse as sp
 
-from abcd_graph.degrees import split_degrees
+from abcd_graph.degrees import assign_degrees, split_degrees
 
 
 @pytest.mark.parametrize("xi", [0.2, 0.4])
@@ -29,3 +29,83 @@ def test_split_degrees(xi):
         np.abs(community_degrees.sum(axis=0) - degrees * (1 - xi))[~is_outlier] < 1
     )
     assert np.all(background_degrees[is_outlier] == degrees[is_outlier])
+
+
+def test_assign_degrees_no_overlap():
+    degrees = np.concatenate(
+        (np.full(16, 4, dtype=np.uint32), np.full(16, 3, dtype=np.uint32))
+    )
+    # Membership matrix with 4 communities size 7 and 4 outliers
+    indptr = np.arange(5, dtype=np.uint32) * 7
+    indices = np.arange(28, dtype=np.uint32)
+    data = np.ones(28, dtype=np.bool)
+    membership_matrix = sp.csr_array((data, indices, indptr), shape=(4, 32))
+    xi = 0.2
+    rng = np.random.default_rng(seed=1)
+
+    assigned_degrees = assign_degrees(
+        degrees,
+        membership_matrix,
+        xi,
+        rng,
+    )
+
+    assert assigned_degrees.shape == degrees.shape
+    assert assigned_degrees.dtype == np.uint32
+    assert np.sum(assigned_degrees == 4) == 16
+    assert np.sum(assigned_degrees == 3) == 16
+
+
+def test_assign_degrees_overlap():
+    degrees = np.concatenate(
+        (np.full(16, 4, dtype=np.uint32), np.full(16, 3, dtype=np.uint32))
+    )
+    # Membership matrix with 4 communities size 7 and 4 outliers
+    indptr = np.arange(6, dtype=np.uint32) * 7
+    indices = np.concatenate((np.arange(28, dtype=np.uint32), np.arange(7) * 4))
+    data = np.ones(35, dtype=np.bool)
+    membership_matrix = sp.csr_array((data, indices, indptr), shape=(5, 32))
+    xi = 0.2
+    rng = np.random.default_rng(seed=1)
+
+    assigned_degrees = assign_degrees(
+        degrees,
+        membership_matrix,
+        xi,
+        rng,
+    )
+
+    assert assigned_degrees.shape == degrees.shape
+    assert assigned_degrees.dtype == np.uint32
+    assert np.sum(assigned_degrees == 4) == 16
+    assert np.sum(assigned_degrees == 3) == 16
+
+
+@pytest.mark.parametrize("xi", [0.2, 0.4])
+@pytest.mark.parametrize("rho", [-0.7, -0.2, 0.2, 0.7])
+def test_assign_degrees_overlap_with_rho(xi, rho):
+    degrees = np.concatenate(
+        (np.full(16, 4, dtype=np.uint32), np.full(16, 3, dtype=np.uint32))
+    )
+    # Membership matrix with 4 communities size 7 and 4 outliers
+    indptr = np.arange(6, dtype=np.uint32) * 7
+    indices = np.concatenate((np.arange(28, dtype=np.uint32), np.arange(7) * 4))
+    data = np.ones(35, dtype=np.bool)
+    membership_matrix = sp.csr_array((data, indices, indptr), shape=(5, 32))
+    rng = np.random.default_rng(seed=1)
+
+    assigned_degrees = assign_degrees(
+        degrees,
+        membership_matrix,
+        xi,
+        rng,
+        rho,
+    )
+
+    assert assigned_degrees.shape == degrees.shape
+    assert assigned_degrees.dtype == np.uint32
+    assert np.sum(assigned_degrees == 4) == 16
+    assert np.sum(assigned_degrees == 3) == 16
+    n_coms = membership_matrix.sum(axis=0)
+    empirical_rho = np.corrcoef(assigned_degrees, n_coms)[0, 1]
+    assert np.sign(empirical_rho) == np.sign(rho)
